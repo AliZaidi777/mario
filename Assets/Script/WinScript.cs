29d4f19c8e8f272b9b58a8cbb4fe5cc51bb2d13a
AliZaidi777 <96346848+AliZaidi777@users.noreply.github.com>
2022-01-14 01:00:15 +0500

    GameOver

--- Assets/Script/WinScript.cs
+++ Assets/Script/WinScript.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class WinScript : MonoBehaviour
 {
+    public GameObject gameover;
     void  OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "mario")
+        if(other.tag == "DTile")
         {
-            SceneManager.LoadScene("win");
+            //SceneManager.LoadScene("win");
+            gameover.SetActive(true);
         }
     }
 }
